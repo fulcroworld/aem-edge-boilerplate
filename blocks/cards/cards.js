83b094b4f40d9dbb0564b84e75467d990514cecb
harshadkulkarni08 <harshad.kulkarni@fulcroworld.com>
2024-09-11 03:12:44 +0530

    Changes in Card Style

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,14 +5,31 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
+
+    const cardWrapper = document.createElement('div');
+    cardWrapper.className = 'cards-card';
+
+    const cardOverlay = document.createElement('div');
+    cardOverlay.className = 'cards-card-overlay';
+
+    cardWrapper.append(cardOverlay);
+
+    while (row.firstElementChild) {
+      cardWrapper.append(row.firstElementChild);
+      li.append(cardWrapper);
+    }
+
+    [...cardWrapper.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+
+      else if (div.className === '') div.className = 'cards-card-body';
     });
+
     ul.append(li);
   });
+
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
   block.textContent = '';
   block.append(ul);
 }
